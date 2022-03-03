11db78fa7c5bb58575564da0bc45c132d77ebfe0
cmyers009 <coopermyers00@gmail.com>
2022-03-02 21:02:38 -0500

    Added support for multiple files

---
+++ multifiletest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:55:33 2022
+
+@author: student
+"""
+
+import pyrealsense_test
+
+
+
+print(pyrealsense_test.main())
--- pyrealsense_test.py
+++ pyrealsense_test.py
@@ -9,8 +9,11 @@ import pyrealsense2 as rs
 import numpy as np
 import matplotlib.pyplot as plt
 
+pipeline=0
+
 def set_min_distance():
-        pipeline = rs.pipeline()
+        global pipeline
+        pipeline= rs.pipeline()
         config = rs.config()
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         profile = pipeline.start(config) # Start streaming
